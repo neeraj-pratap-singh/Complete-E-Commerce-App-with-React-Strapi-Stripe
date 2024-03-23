214610ada98f087deeb4f7936253991b9c9353ce
neeraj-singh <71137350+neeraj-pratap-singh@users.noreply.github.com>
2024-03-23 10:32:51 +0530

    Basic folder setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,46 @@
-import './App.css';
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+import Footer from "./components/Footer/Footer";
+import Navbar from "./components/Navbar/Navbar";
+import Home from "./pages/Home/Home";
+import Product from "./pages/Product/Product";
+import Products from "./pages/Products/Products";
+import "./App.css"
+
+const Layout = () => {
+  return (
+    <div className="app">
+      <Navbar />
+      <Outlet />
+      <Footer />
+    </div>
+  );
+};
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/products/:id",
+        element: <Products />,
+      },
+      {
+        path: "/product/:id",
+        element: <Product />,
+      },
+    ],
+  },
+]);
 
 function App() {
   return (
-    <div className="App">
-      Hello
+    <div>
+      <RouterProvider router={router} />
     </div>
   );
 }
